d3a1e82d9870f8429c4af3debe2348b9894ad51c
jason <guangweidu@qq.com>
2018-03-14 01:25:56 +0800

    init

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,25 @@
+Page({
+  data: {
+    images: [{
+      url: 'https://lc-i0j7ktvk.cn-n1.lcfile.com/fee5cf3564ad1b30dbbc.jpg',
+      width: 700,
+      height: 500,
+      x: 25,
+      y: 25,
+    },
+    {
+      url: 'https://lc-i0j7ktvk.cn-n1.lcfile.com/8eb44bfd6dd7e94e1b51.png',
+      width: 175,
+      height: 175,
+      x: 550,
+      y: 550,
+    }
+    ],
+    width: 750,
+    height: 750,
+    backgroundColor: '#ccc',
+    debug: true,
+  },
+  onLoad: function () {
+  }
+})
---
+++ plugin/components/canvas/canvas.js
@@ -0,0 +1,135 @@
+Component({
+    properties: {
+        images: {
+            type: Array,
+            value: [{
+                url: '',
+                width: 0,
+                height: 0,
+                x: 0,
+                y: 0,
+            }],
+        },
+        width: {
+            type: Number,
+            value: 750,
+        },
+        height: {
+            type: Number,
+            value: 750,
+        },
+        backgroundColor: {
+            type: String,
+            value: '#ffffff',
+        },
+        debug: {
+            type: Boolean,
+            value: false,
+        },
+    },
+    data: {
+    },
+    created() {
+        const sysInfo = wx.getSystemInfoSync();
+        const screenWidth = sysInfo.screenWidth;
+        // rpx（responsive pixel）: 规定屏幕宽为750rpx, 所以只需关心rpx即可
+        this.factor = screenWidth / 750;
+    },
+    attached() {
+        this.ctx = wx.createCanvasContext('imgCanvas', this);
+        this.ctx.setFillStyle(this.data.backgroundColor);
+        this.ctx.fillRect(0, 0, this.toPx(this.data.width), this.toPx(this.data.height));
+        this.ctx.draw(true);
+    },
+    methods: {
+        create() {
+            const { images } = this.data;
+            const drawList = [];
+            images.forEach((image) => drawList.push(this.drawImage(image)));
+            Promise.all(drawList)
+                .then(() => {
+                    return this.toImage(this.ctx);
+                })
+                .then((imgPath) => {
+                    wx.previewImage({
+                        urls: [imgPath],
+                    });
+                })
+        },
+        toImage(ctx) {
+            return new Promise((resolve, reject) => {
+                wx.canvasToTempFilePath({
+                    canvasId: 'imgCanvas',
+                    success(res) {
+                        resolve(res.tempFilePath);
+                    },
+                    fail(err) {
+                        reject(err);
+                    }
+                }, this)
+            });
+        },
+        drawImage(image) {
+            return new Promise((resolve, reject) => {
+                const { x, y, url } = image;
+                this.downImage(url)
+                    .then((imgPath) => { return this.getImageInfo(imgPath); })
+                    .then(({ imgPath, imgInfo }) => {
+                        let sx;
+                        let sy;
+                        const setWidth = image.width;
+                        const setHeight = image.height;
+                        const width = imgInfo.width;
+                        const height = imgInfo.height;
+
+                        if (width / height <= setWidth / setHeight) {
+                            sx = 0;
+                            sy = (height - ((width / setWidth) * setHeight)) / 2;
+                        } else {
+                            sy = 0;
+                            sx = (width - ((height / setHeight) * setWidth)) / 2;
+                        }
+                        this.ctx.drawImage(imgPath,
+                            sx, sy, (width - (sx * 2)), (height - (sy * 2)),
+                            this.toPx(x), this.toPx(y), this.toPx(setWidth), this.toPx(setHeight));
+                        this.ctx.draw(true);
+                        resolve();
+                    })
+                    .catch((err) => reject(err));
+            });
+        },
+        downImage(imageUrl) {
+            return new Promise((resolve, reject) => {
+                wx.downloadFile({
+                    url: imageUrl,
+                    success(res) {
+                        if (res.statusCode === 200) {
+                            resolve(res.tempFilePath);
+                        } else {
+                            reject(res.errMsg);
+                        }
+                    },
+                    fail(err) {
+                        reject(err);
+                    }
+                });
+            })
+        },
+        getImageInfo(imgPath) {
+            return new Promise((resolve, reject) => {
+                wx.getImageInfo({
+                    src: imgPath,
+                    success(res) {
+                        resolve({ imgPath, imgInfo: res });
+                    },
+                    fail(err) {
+                        reject(err);
+                    }
+                });
+            });
+        },
+        toPx(rpx) {
+            return rpx * this.factor;
+        }
+    }
+})
