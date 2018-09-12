06cb095a8591ed985dd53a9da34c23a3d92d4d5f
jasondu <duguangwei@youzan.com>
2018-09-12 11:22:18 +0800

    add preload and hide loading options

--- miniprogram_dist/poster/index.js
+++ miniprogram_dist/poster/index.js
@@ -3,12 +3,73 @@ Component({
         config: {
             type: Object,
             value: {},
+        },
+        preload: {  // 是否预下载图片资源
+            type: Boolean,
+            value: false,
+        },
+        hideLoading: {  // 是否隐藏loading
+            type: Boolean,
+            value: false,
+        }
+    },
+    ready() {
+        if (this.data.preload) {
+            const poster = this.selectComponent('#poster');
+            this.downloadStatus = 'doing';
+            poster.downloadResource(this.data.config.images).then(() => {
+                this.downloadStatus = 'success';
+                this.trigger('downloadSuccess');
+            }).catch((e) => {
+                this.downloadStatus = 'fail';
+                this.trigger('downloadFail', e);
+            });
         }
     },
     methods: {
+        trigger(event, data) {
+            if (this.listener && typeof this.listener[event] === 'function') {
+                this.listener[event](data);
+            }
+        },
+        once(event, fun) {
+            if (typeof this.listener === 'undefined') {
+                this.listener = {};
+            }
+            this.listener[event] = fun;
+        },
+        downloadResource() {
+            return new Promise((resolve, reject) => {
+                const poster = this.selectComponent('#poster');
+                if (this.downloadStatus && this.downloadStatus !== 'fail') {
+                    if (this.downloadStatus === 'success') {
+                        resolve();
+                    } else {
+                        this.once('downloadSuccess', () => resolve());
+                        this.once('downloadFail', (e) => reject(e));
+                    }
+                } else {
+                    poster.downloadResource(this.data.config.images)
+                        .then(() => {
+                            this.downloadStatus = 'success';
+                            resolve();
+                        })
+                        .catch((e) => reject(e));
+                }  
+            })
+        },
         onCreate() {
-            const poster = this.selectComponent('#poster');
-            poster.create(this.data.config);
+            !this.data.hideLoading && wx.showLoading({ mask: true, title: '生成中' });
+            return this.downloadResource().then(() => {
+                this.data.hideLoading && wx.hideLoading();
+                const poster = this.selectComponent('#poster');
+                poster.create(this.data.config);
+            })
+            .catch((err) => {
+                !this.data.hideLoading && wx.hideLoading();
+                wx.showToast({ icon: 'none', title: err.errMsg || '生成失败' });
+                console.error(err);
+            })
         },
         onCreateSuccess(e) {
             const { detail } = e;
--- pages/index/index.js
+++ pages/index/index.js
@@ -8,7 +8,154 @@ Page({
             height: 1334,
             backgroundColor: '#fff',
             debug: false,
-},
+            blocks: [
+                {
+                    width: 690,
+                    height: 808,
+                    x: 30,
+                    y: 183,
+                    borderWidth: 2,
+                    borderColor: '#f0c2a0',
+                    borderRadius: 20,
+                },
+                {
+                    width: 634,
+                    height: 74,
+                    x: 59,
+                    y: 770,
+                    backgroundColor: '#fff',
+                    opacity: 0.5,
+                    zIndex: 100,
+                },
+            ],
+            texts: [
+                {
+                    x: 113,
+                    y: 61,
+                    baseLine: 'middle',
+                    text: '伟仔',
+                    fontSize: 32,
+                    color: '#8d8d8d',
+                },
+                {
+                    x: 30,
+                    y: 113,
+                    baseLine: 'top',
+                    text: '发现一个好物，推荐给你呀',
+                    fontSize: 38,
+                    color: '#080808',
+                },
+                {
+                    x: 92,
+                    y: 810,
+                    fontSize: 38,
+                    baseLine: 'middle',
+                    text: '标题标题标题标题标题标题标题标题标题',
+                    width: 570,
+                    lineNum: 1,
+                    color: '#8d8d8d',
+                    zIndex: 200,
+                },
+                {
+                    x: 59,
+                    y: 895,
+                    baseLine: 'middle',
+                    text: [
+                        {
+                            text: '2人拼',
+                            fontSize: 28,
+                            color: '#ec1731',
+                        },
+                        {
+                            text: '¥99',
+                            fontSize: 36,
+                            color: '#ec1731',
+                            marginLeft: 30,
+                        }
+                    ]
+                },
+                {
+                    x: 522,
+                    y: 895,
+                    baseLine: 'middle',
+                    text: '已拼2件',
+                    fontSize: 28,
+                    color: '#929292',
+                },
+                {
+                    x: 59,
+                    y: 945,
+                    baseLine: 'middle',
+                    text: [
+                        {
+                            text: '商家发货&售后',
+                            fontSize: 28,
+                            color: '#929292',
+                        },
+                        {
+                            text: '七天退货',
+                            fontSize: 28,
+                            color: '#929292',
+                            marginLeft: 50,
+                        },
+                        {
+                            text: '运费险',
+                            fontSize: 28,
+                            color: '#929292',
+                            marginLeft: 50,
+                        },
+                    ]
+                },
+                {
+                    x: 360,
+                    y: 1065,
+                    baseLine: 'top',
+                    text: '长按识别小程序码',
+                    fontSize: 38,
+                    color: '#080808',
+                },
+                {
+                    x: 360,
+                    y: 1123,
+                    baseLine: 'top',
+                    text: '超值好货一起拼',
+                    fontSize: 28,
+                    color: '#929292',
+                },
+            ],
+            images: [
+                {
+                    width: 62,
+                    height: 62,
+                    x: 30,
+                    y: 30,
+                    borderRadius: 62,
+                    url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/02bb99132352b5b5dcea.jpg',
+                },
+                {
+                    width: 634,
+                    height: 634,
+                    x: 59,
+                    y: 210,
+                    url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/193256f45999757701f2.jpeg',
+                },
+                {
+                    width: 220,
+                    height: 220,
+                    x: 92,
+                    y: 1020,
+                    url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/d719fdb289c955627735.jpg',
+                },
+                {
+                    width: 750,
+                    height: 90,
+                    x: 0,
+                    y: 1244,
+                    url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/67b0a8ad316b44841c69.png',
+                }
+            ]
+
+        },
         demoConfig: {
             width: 750,
             height: 1000,
@@ -136,154 +283,6 @@ Page({
      * 异步生成海报
      */
     onCreatePoster() {
-        this.data.jdConfig.blocks = [
-            {
-                width: 690,
-                height: 808,
-                x: 30,
-                y: 183,
-                borderWidth: 2,
-                borderColor: '#f0c2a0',
-                borderRadius: 20,
-            },
-            {
-                width: 634,
-                height: 74,
-                x: 59,
-                y: 770,
-                backgroundColor: '#fff',
-                opacity: 0.5,
-                zIndex: 100,
-            },
-        ],
-        this.data.jdConfig.texts = [
-            {
-                x: 113,
-                y: 61,
-                baseLine: 'middle',
-                text: '伟仔',
-                fontSize: 32,
-                color: '#8d8d8d',
-            },
-            {
-                x: 30,
-                y: 113,
-                baseLine: 'top',
-                text: '发现一个好物，推荐给你呀',
-                fontSize: 38,
-                color: '#080808',
-            },
-            {
-                x: 92,
-                y: 810,
-                fontSize: 38,
-                baseLine: 'middle',
-                text: '标题标题标题标题标题标题标题标题标题',
-                width: 570,
-                lineNum: 1,
-                color: '#8d8d8d',
-                zIndex: 200,
-            },
-            {
-                x: 59,
-                y: 895,
-                baseLine: 'middle',
-                text: [
-                    {
-                        text: '2人拼',
-                        fontSize: 28,
-                        color: '#ec1731',
-                    },
-                    {
-                        text: '¥99',
-                        fontSize: 36,
-                        color: '#ec1731',
-                        marginLeft: 30,
-                    }
-                ]
-            },
-            {
-                x: 522,
-                y: 895,
-                baseLine: 'middle',
-                text: '已拼2件',
-                fontSize: 28,
-                color: '#929292',
-            },
-            {
-                x: 59,
-                y: 945,
-                baseLine: 'middle',
-                text: [
-                    {
-                        text: '商家发货&售后',
-                        fontSize: 28,
-                        color: '#929292',
-                    },
-                    {
-                        text: '七天退货',
-                        fontSize: 28,
-                        color: '#929292',
-                        marginLeft: 50,
-                    },
-                    {
-                        text: '运费险',
-                        fontSize: 28,
-                        color: '#929292',
-                        marginLeft: 50,
-                    },
-                ]
-            },
-            {
-                x: 360,
-                y: 1065,
-                baseLine: 'top',
-                text: '长按识别小程序码',
-                fontSize: 38,
-                color: '#080808',
-            },
-            {
-                x: 360,
-                y: 1123,
-                baseLine: 'top',
-                text: '超值好货一起拼',
-                fontSize: 28,
-                color: '#929292',
-            },
-        ],
-        this.data.jdConfig.images = [
-            {
-                width: 62,
-                height: 62,
-                x: 30,
-                y: 30,
-                borderRadius: 62,
-                url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/02bb99132352b5b5dcea.jpg',
-            },
-            {
-                width: 634,
-                height: 634,
-                x: 59,
-                y: 210,
-                url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/193256f45999757701f2.jpeg',
-            },
-            {
-                width: 220,
-                height: 220,
-                x: 92,
-                y: 1020,
-                url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/d719fdb289c955627735.jpg',
-            },
-            {
-                width: 750,
-                height: 90,
-                x: 0,
-                y: 1244,
-                url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/67b0a8ad316b44841c69.png',
-            }
-        ]
-        this.setData({
-            jdConfig: this.data.jdConfig
-        }, () => Poster.create());
+        Poster.create();
     }
 })
