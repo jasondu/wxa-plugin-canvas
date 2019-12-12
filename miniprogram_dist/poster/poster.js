f1521019783a3ec04ca7822a15926b344bed6c54
LeachZhou <leachzhou@163.com>
2019-12-12 15:14:18 +0800

    fix:修复在自定义组件中使用时已正确设置id提示“请设置组件的id="poster"!!!”

--- miniprogram_dist/poster/poster.js
+++ miniprogram_dist/poster/poster.js
@@ -2,27 +2,27 @@ const defaultOptions = {
     selector: '#poster'
 };
 
-function Poster(options = {}) {
+function Poster(options = {}, that) {
     options = {
         ...defaultOptions,
         ...options,
     };
 
     const pages = getCurrentPages();
-    const ctx = pages[pages.length - 1];
-
+    let ctx = pages[pages.length - 1];
+    if (that) ctx = that
     const poster = ctx.selectComponent(options.selector);
     delete options.selector;
 
     return poster;
 };
 
-Poster.create = (reset = false) => {
-    const poster  = Poster();
+Poster.create = (reset = false, that) => {
+    const poster  = Poster({}, that);
     if (!poster) {
         console.error('请设置组件的id="poster"!!!');
     } else {
-        return Poster().onCreate(reset);
+        return Poster({}, that).onCreate(reset);
     }
 }
 
