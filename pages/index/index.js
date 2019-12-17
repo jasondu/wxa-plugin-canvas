8b436e22003dafacc2fc0df58321868e9be2709c
guangwei.du <guangwei.du@lazada.com>
2019-12-17 20:12:48 +0800

    fix(fixed some bug):

--- pages/index/index.js
+++ pages/index/index.js
@@ -110,9 +110,13 @@ const posterConfig = {
                 x: 360,
                 y: 1065,
                 baseLine: 'top',
-                text: '长按识别小程序码',
-                fontSize: 38,
+                text: '长按识别小程序码长按识别小程序码长按识别小程序码',
+                width: 700,
+                // lineHeight: 40,
+                lineNum: 3,
+                fontSize: 40,
                 color: '#080808',
+                baseLine: 'middle'
             },
             {
                 x: 360,
@@ -157,114 +161,24 @@ const posterConfig = {
 
     },
     demoConfig: {
-        width: 750,
-        height: 1000,
-        backgroundColor: '#fff',
-        debug: false,
-        blocks: [
-            {
-                x: 0,
-                y: 10,
-                width: 750, // 如果内部有文字，由文字宽度和内边距决定
-                height: 120,
-                paddingLeft: 0,
-                paddingRight: 0,
-                borderWidth: 10,
-                borderColor: 'red',
-                backgroundColor: 'blue',
-                borderRadius: 40,
-                text: {
-                    text: [
-                        {
-                            text: '金额¥ 1.00',
-                            fontSize: 80,
-                            color: 'yellow',
-                            opacity: 1,
-                            marginLeft: 50,
-                            marginRight: 10,
-                        },
-                        {
-                            text: '金额¥ 1.00',
-                            fontSize: 20,
-                            color: 'yellow',
-                            opacity: 1,
-                            marginLeft: 10,
-                            textDecoration: 'line-through',
-                        },
-                    ],
-                    baseLine: 'middle',
-                },
-            }
-        ],
+      width: 750,
+      height: 1220,
+      backgroundColor: '#fff',
+      debug: false,
+      pixelRatio: 2,
         texts: [
-            {
-                x: 0,
-                y: 180,
-                text: [
-                    {
-                        text: '长标题长标题长标题长标题长标题长标题长标题长标题长标题',
-                        fontSize: 40,
-                        color: 'red',
-                        opacity: 1,
-                        marginLeft: 0,
-                        marginRight: 10,
-                        width: 200,
-                        lineHeight: 40,
-                        lineNum: 2,
-                    },
-                    {
-                        text: '原价¥ 1.00',
-                        fontSize: 40,
-                        color: 'blue',
-                        opacity: 1,
-                        marginLeft: 10,
-                        textDecoration: 'line-through',
-                    },
-                ],
-                baseLine: 'middle',
-            },
-            {
-                x: 10,
-                y: 330,
-                text: '金额¥ 1.00',
-                fontSize: 80,
-                color: 'blue',
-                opacity: 1,
-                baseLine: 'middle',
-                textDecoration: 'line-through',
-            },
+          {
+            x: 250,
+            y: 580,
+            baseLine: 'middle',
+            text: '测试测试测试测试测试测试测试测试测试测试测试测试测试',
+            fontSize: 40,
+            color: '#000000',
+            // textAlign: 'center',
+            lineNum: 3,
+            width: 300
+          },
         ],
-        images: [
-            {
-                url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/02bb99132352b5b5dcea.jpg',
-                width: 300,
-                height: 300,
-                y: 450,
-                x: 0,
-                // borderRadius: 150,
-                // borderWidth: 10,
-                // borderColor: 'red',
-            },
-            {
-                url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/02bb99132352b5b5dcea.jpg',
-                width: 100,
-                height: 100,
-                y: 450,
-                x: 400,
-                borderRadius: 100,
-                borderWidth: 10,
-            },
-        ],
-        lines: [
-            {
-                startY: 800,
-                startX: 10,
-                endX: 300,
-                endY: 800,
-                width: 5,
-                color: 'red',
-            }
-        ]
 
     }
 }
